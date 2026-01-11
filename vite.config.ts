3201b3ec0168c0545bfe254ab332a6a44156e2cf
WooBin Lee <woobina@gmail.com>
2026-01-11 11:47:19 +0900

    Add GitHub Pages deployment workflow

--- vite.config.ts
+++ vite.config.ts
@@ -207,6 +207,7 @@ function cdnPrefixImages(): Plugin {
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   return {
+    base: process.env.GITHUB_PAGES ? '/burgergo/' : '/',
     server: {
       host: "::",
       port: 8080,
